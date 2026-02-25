8a34af8ed33b016973e109f1a11fd0cae30daf02
BecarioAlamex <becario@alam.mx>
2026-02-25 13:44:19 -0600

    Delete vitejs-vite-ub9eztbx directory

--- vitejs-vite-ub9eztbx/src/supabaseClient.ts
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-// AQUÍ VAMOS A PEGAR TUS CLAVES SECRETAS DE SUPABASE
-const supabaseUrl = 'https://bddsykxhwfgumianzxqp.supabase.co';
-const supabaseAnonKey = 'sb_publishable_PPRZmZHaTYZdyTDot_wmKQ_URkCO9Zc';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
